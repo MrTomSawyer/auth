8914d31efaf1dbbbe376c3002e8f1907a966d39e
Slava V <letterthing@gmail.com>
2024-02-27 16:25:00 +0700

    graceful shutdown

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -4,6 +4,9 @@ import (
 	"github.com/MrTomSawyer/sso/internal/app"
 	"github.com/MrTomSawyer/sso/internal/config"
 	"github.com/MrTomSawyer/sso/internal/logger"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -12,5 +15,13 @@ func main() {
 
 	server := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
 
-	server.GRPCServer.MustRun()
+	go server.GRPCServer.MustRun()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	<-stop
+
+	server.GRPCServer.Stop()
+	log.Info("server stopped")
 }
